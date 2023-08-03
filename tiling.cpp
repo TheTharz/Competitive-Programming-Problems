5f0f9d924a9eb8fdde53fc846c0c82f2ecad02be
thetharz <tharinduimalka915@gmail.com>
2023-08-03 13:55:07 +0530

    tiling

--- tiling.cpp
+++ tiling.cpp
@@ -3,14 +3,34 @@
 using namespace std;
 int main(){
   int n;
-  cin >> n;
-
-  int arr[8][n+1];
+  while(true){
+    cin >> n;
+    if(n==-1){
+      break;
+    }else{
+      int arr[8][n+1];
   memset(arr, 0, sizeof(arr));
+  arr[7][0] = 1;
 
-  for(int i = 0; i < 8; i++){
-    cout << arr[i][0] <<endl;
+  for(int i = 1; i <= n; i++){
+    arr[0][i] += arr[7][i-1];
+    arr[1][i] += arr[6][i-1];
+    arr[2][i] += arr[5][i-1];
+    arr[3][i] += arr[7][i-1];
+    arr[3][i] += arr[4][i-1];
+    arr[4][i] += arr[3][i-1];
+    arr[5][i] += arr[2][i-1];
+    arr[6][i] += arr[7][i-1];
+    arr[6][i] += arr[1][i-1];
+    arr[7][i] += arr[0][i-1];
+    arr[7][i] += arr[3][i-1];
+    arr[7][i] += arr[6][i-1];
+    
+   // arr[0][i] += arr[7][i-1];
   }
-
+  cout <<arr[7][n] << endl;
+  }
+    }
+  
   return 0;
 }
